d229a5a4237cdecff1d04be9524751c1fb7db02c
VadikoS <84492554+VadikoS@users.noreply.github.com>
2021-07-09 17:33:23 +0300

    Add files via upload

---
+++ lab3/lab3/lab3/Cache.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace Cache
+{
+    class Cache<T>
+    {
+        private TimeSpan time;
+        private int size;
+        private Dictionary<string, Tuple<T, DateTime>> memory;
+
+        public Cache(TimeSpan tm, int sz)
+        {
+            time = tm;
+            size = sz;
+            memory = new Dictionary<string, Tuple<T, DateTime>>();
+        }
+        public void Save(string key, T data)
+        {
+            Tuple<T, DateTime> val;
+            DeleteOldCache();
+            if (memory.TryGetValue(key, out val))
+            {
+                throw new ArgumentException();
+            }
+            else if (memory.Count == size)
+            {
+                TimeSpan diff = new TimeSpan(0);
+                DateTime curr = DateTime.Now;
+                DateTime create;
+                string max_key = null;
+                foreach (var elem in memory)
+                {
+                    create = elem.Value.Item2;
+                    if (curr - create > diff)
+                    {
+                        diff = curr - create;
+                        max_key = elem.Key;
+                    }
+                }
+                memory.Remove(max_key);
+                memory.Add(key, new Tuple<T, DateTime>(data, DateTime.Now));
+            }
+            else
+            {
+                memory.Add(key, new Tuple<T, DateTime>(data, DateTime.Now));
+            }
+        }
+        public T Get(string key)
+        {
+            Tuple<T, DateTime> data;
+            DeleteOldCache();
+            if (!memory.TryGetValue(key, out data))
+            {
+                throw new KeyNotFoundException();
+            }
+            return data.Item1;
+        }
+        private void DeleteOldCache()
+        {
+            TimeSpan diff;
+            DateTime curr, create;
+            foreach (var elem in memory)
+            {
+                curr = DateTime.Now;
+                create = elem.Value.Item2;
+                diff = curr - create;
+                if (diff >= time)
+                {
+                    memory.Remove(elem.Key);
+                }
+            }
+        }
+    }
+}
---
+++ lab3/lab3/lab3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Cache;
+using System.Threading;
+
+namespace lab3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TimeSpan        time = new TimeSpan(0, 1, 0, 0, 0);
+            Cache<string>   cache = new Cache<string>(time, 10);
+            
+            cache.Save("123", "wrkqrlkqrjlkqjrlkqjrlkqjrlqkjr");
+            cache.Save("111", "messagemessage");
+            cache.Save("11231", "mai"); 
+            cache.Save("24044", "wqr11231312312");
+            cache.Save("1231", "qqqqqqqqqqqqqq");
+
+            Console.Write(cache.Get("1231"));
+            try
+            {
+                Console.Write(cache.Get("12313132"));
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
